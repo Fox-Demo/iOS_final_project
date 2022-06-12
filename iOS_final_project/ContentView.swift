0ac80074b49e8cf57ff55735c35bdba54f1ccedd
LI-YONG-QI <a2369028@gmail.com>
2022-06-12 22:22:36 +0800

    Initial Commit

---
+++ iOS_final_project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_final_project
+//
+//  Created by li on 2022/6/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_final_project/iOS_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_final_projectApp.swift
+//  iOS_final_project
+//
+//  Created by li on 2022/6/12.
+//
+
+import SwiftUI
+
+@main
+struct iOS_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
